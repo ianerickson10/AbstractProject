678da4328a148378468f686bd8315c99aedead03
Ian Erickson <19ierickson@gmail.com>
2018-12-06 12:44:35 -0700

    added methods to AbstractionViewController

--- AbstractProject/Controller/AbstractionViewController.swift
+++ AbstractProject/Controller/AbstractionViewController.swift
@@ -8,7 +8,40 @@
 
 import UIKit
 
-class AbstractionViewController: UIPageViewController
+public class AbstractionViewController: UIPageViewController, UIPageViewControllerDataSource
 {
     
+    //MARK:- Data Members
+    private lazy var orderedAbstractionViews : [UIViewController] =
+    {
+        return [
+            self.newAbstractionViewController(abstractionLevel: "Swift"),
+            self.newAbstractionViewController(abstractionLevel: "Block"),
+            self.newAbstractionViewController(abstractionLevel: "ByteCode"),
+            self.newAbstractionViewController(abstractionLevel: "Binary"),
+            self.newAbstractionViewController(abstractionLevel: "LogicalGate")
+        ]
+    }()
+    
+    //MARK: Helper method to get correct ViewController
+    private func newAbstractionViewController(abstractionLevel : String) -> UIViewController
+    {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\
+            (abstractionLevel)ViewController")
+    }
+    
+    //MARK:- Lifecycle Methods
+    public override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        dataSource = self
+        
+        if let firstViewController = orderedAbstractionViews.first
+        {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated:  true,
+                               completion: nil)
+        }
+    }
 }
